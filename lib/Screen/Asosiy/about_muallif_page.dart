2420fdb8dc80467c107fab304cad7f876b109a96
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2025-09-19 19:30:49 +0500

    asdf

--- lib/Screen/Asosiy/about_muallif_page.dart
+++ lib/Screen/Asosiy/about_muallif_page.dart
@@ -55,119 +55,130 @@ class _AboutMuallifPageState extends State<AboutMuallifPage> {
             color: Colors.white,
             borderRadius: BorderRadius.circular(8.r),
           ),
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 4.h),
-                height: 520.h,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    // Profil rasmi
-                    CircleAvatar(
-                      radius: 50.r,
-                      backgroundImage: AssetImage("assets/images/Ellipse.png"),
-                    ),
-                    SizedBox(height: 12.h),
-
-                    // Ism
-                    Text(
-                      "Sardor Qodirov",
-                      style: TextStyle(
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.w700,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 4.h),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // Profil rasmi
+                      CircleAvatar(
+                        radius: 50.r,
+                        backgroundImage: AssetImage("assets/images/Ellipse.png"),
                       ),
-                    ),
-                    SizedBox(height: 12.h),
-
-                    // Lavozim
-                    Text(
-                      "Full stack dizayner",
-                      style: TextStyle(
-                        fontSize: 14.sp,
-                        color: Colors.grey.shade600,
-                      ),
-                    ),
-                    SizedBox(height: 24.h),
-
-                    // Statistika
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "915",
-                              style: TextStyle(
-                                fontSize: 22.sp,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              "O‘quvchilar",
-                              style: TextStyle(
-                                fontSize: 13.sp,
-                                color: Colors.grey.shade600,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(width: 50.w),
-                        Column(
-                          children: [
-                            Text(
-                              "22",
-                              style: TextStyle(
-                                fontSize: 25.sp,
-                                fontWeight: FontWeight.w500,
-                                fontFamily: 'Regular',
-                              ),
-                            ),
-                            Text(
-                              "Izohlar",
-                              style: TextStyle(
-                                fontSize: 13.sp,
-                                color: Colors.grey.shade600,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 24.h),
-
-                    // Bio matn
-                    Text(
-                      "Ismim Sardor Qodirov (KADIROV). Grafik va veb dizaynerman.\n\n"
-                      "Bu yerda siz Photoshop dasturida hosil qilinadigan ajoyib darsliklarni ko‘rishingiz mumkin. "
-                      "Har xil prikol, uylas, g‘iybat va shunga o‘xshash keraksiz, vaqtni oladigan videolarni ko‘rmasdan, "
-                      "o‘zingizni rivojlantirish, hayotingizni yaxshi tomonga o‘zgartirish, yangi bilimlarni o‘zlashtirish uchun "
-                      "ushbu kanalga kirganingiz uchun rahmat!",
-                      style: TextStyle(
-                        fontSize: 14.sp,
-                        color: Color.fromRGBO(57, 60, 67, 1),
-                        fontFamily: 'Regular',
-                        height: 1.5,
-                      ),
-                    ),
-                    SizedBox(height: 30.h),
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'Dasturlash bo’yicha',
+                      SizedBox(height: 12.h),
+            
+                      // Ism
+                      Text(
+                        "Sardor Qodirov",
                         style: TextStyle(
-                          color: Color.fromRGBO(13, 20, 39, 1),
-                          fontSize: 20.sp,
+                          fontSize: 24.sp,
                           fontWeight: FontWeight.w600,
-                          fontFamily: 'Medium',
+                          fontFamily: 'Regular',
+                          color: Color.fromRGBO(13, 20, 39, 1),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 12.h),
+            
+                      // Lavozim
+                      Text(
+                        "Full stack dizayner",
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          color: Color.fromRGBO(95, 100, 110, 1),
+                          fontFamily: 'Regular',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(height: 24.h),
+            
+                      // Statistika
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "O‘quvchilar",
+                                style: TextStyle(
+                                  fontSize: 12.sp,
+                                  color: Color.fromRGBO(165, 166, 168, 1),
+                                  fontFamily: 'Regular',
+                                ),
+                              ),
+                              Text(
+                                "915",
+                                style: TextStyle(
+                                  fontSize: 25.sp,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color.fromRGBO(13, 20, 39, 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(width: 30.w),
+                          Column(
+                            children: [
+                              Text(
+                                "Izohlar",
+                                style: TextStyle(
+                                  fontSize: 12.sp,
+                                  color: Color.fromRGBO(165, 166, 168, 1),
+                                  fontFamily: 'Regular',
+                                ),
+                              ),
+                              Text(
+                                "22",
+                                style: TextStyle(
+                                  fontSize: 25.sp,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color.fromRGBO(13, 20, 39, 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 24.h),
+            
+                      // Bio matn
+                      Text(
+                        "Ismim Sardor Qodirov (KADIROV). Grafik va veb dizaynerman.\n\n"
+                        "Bu yerda siz Photoshop dasturida hosil qilinadigan ajoyib darsliklarni ko‘rishingiz mumkin. "
+                        "Har xil prikol, uylas, g‘iybat va shunga o‘xshash keraksiz, vaqtni oladigan videolarni ko‘rmasdan, "
+                        "o‘zingizni rivojlantirish, hayotingizni yaxshi tomonga o‘zgartirish, yangi bilimlarni o‘zlashtirish uchun "
+                        "ushbu kanalga kirganingiz uchun rahmat!",
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          color: Color.fromRGBO(57, 60, 67, 1),
+                          fontFamily: 'Regular',
+                          height: 1.5,
+                        ),
+                      ),
+                      SizedBox(height: 30.h),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Dasturlash bo’yicha',
+                          style: TextStyle(
+                            color: Color.fromRGBO(13, 20, 39, 1),
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Medium',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(child: KursBannerClass().kursGrid("1 200 000")),
-            ],
+                SizedBox(
+                  child: KursBannerClass().kursGrid("1 200 000"),
+                  height: 600,
+                ),
+              ],
+            ),
           ),
         ),
       ),
